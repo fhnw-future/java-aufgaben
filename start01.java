7ac6a45e597d72998f9634e422ae944c84ede9ae
flo7000 <95176997+flo7000@users.noreply.github.com>
2025-02-24 11:55:45 +0100

    initial commit

---
+++ start01.java
@@ -0,0 +1,15 @@
+public class start01 {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        double a = 1;
+        double b = 5;
+        double sol = a / b * 1;
+        System.out.println("double sol equals: " + sol);
+
+    }
+    
+    
+}
